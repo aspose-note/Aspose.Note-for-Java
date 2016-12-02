0f4cb1e7f5ea7f09c23a70e95762336a2b5d0022
Fahad Adeel <fahadadeel@gmail.com>
2016-12-02 12:08:15 +0500

    Aspose.Note for Java 16.11.0 release examples

---
+++ Examples/src/main/java/com/aspose/note/examples/Notebook/SaveNotebooktoStream.java
@@ -0,0 +1,33 @@
+package com.aspose.note.examples.Notebook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.aspose.note.Document;
+import com.aspose.note.Notebook;
+import com.aspose.note.examples.Utils;
+
+public class SaveNotebooktoStream {
+	public static void main(String... args) throws IOException {
+		// Load the document into Aspose.Note.
+		String dataDir = Utils.getSharedDataDir(SupportofPasswordProtectedDocuments.class) + "Notebook/";
+
+		Notebook notebook = new Notebook();
+
+		FileOutputStream stream = new FileOutputStream(dataDir + "output.onetoc2");
+
+		notebook.save(stream);
+
+		if (notebook.getCount() > 0) {
+			Document childDocument0 = (Document) notebook.get_Item(0);
+
+			OutputStream childStream = new FileOutputStream(dataDir + "childStream.one");
+			childDocument0.save(childStream);
+
+			Document childDocument1 = (Document) notebook.get_Item(1);
+
+			childDocument1.save(dataDir + "child.one");
+		}
+	}
+}
