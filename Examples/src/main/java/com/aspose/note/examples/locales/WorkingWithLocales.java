3e81328f4270197597ad9c36ea04f0cde52131f1
muzammil-khan <muzammil.khan@aspose.com>
2019-04-17 19:25:40 +0500

    Updates for Aspose.Note for Java 19.4 Release.

---
+++ Examples/src/main/java/com/aspose/note/examples/locales/WorkingWithLocales.java
@@ -0,0 +1,30 @@
+package com.aspose.note.examples.locales;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Locale;
+
+import com.aspose.note.Document;
+import com.aspose.note.License;
+import com.aspose.note.LocaleOptions;
+import com.aspose.note.examples.Utils;
+import com.aspose.note.examples.load.SaveToStream;
+
+public class WorkingWithLocales {
+
+	public static void main(String[] args) throws IOException {
+		License license = new License();
+	    license.setLicense("licenseFile");
+		 
+	    java.util.Locale.setDefault(new java.util.Locale("en", "rs"));
+	    LocaleOptions.setLocale(Locale.US);
+		
+	    String inputFile = "Sample1.one";
+        Path inputPath = Utils.getPath(SaveToStream.class, inputFile);
+
+        // Load the document into Aspose.Note
+        Document oneFile = new Document(inputPath.toString());
+        oneFile.save("sample.png");
+	}
+
+}
